92ad487d9529b3528eddc2ffeccea7272b1f7caa
EfeDeveloper <edwinvilla_adsi@outlook.com>
2021-05-29 08:53:01 -0500

    ➕ ADD : Se agrega navbar, react-router y react-fire al proyecto asi como la config de firebase para el registro y login

---
+++ src/components/registerCow.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RegisterCow() {
+    return (
+        <div>
+            <h1>Estas en register Cow</h1>
+        </div>
+    )
+}
+
+export default RegisterCow
